1436a81bb1782f7081dc76069a0680f1f9a985ab
Inquieto <nicolanki036@gmail.com>
2026-02-05 03:42:38 -0300

    Delete JavaScript.js

--- JavaScript.js
+++
@@ -1,7 +0,0 @@
-<script>
-fetch("https://discord-backend-1.onrender.com/contador")
-  .then(r => r.json())
-  .then(data => {
-    document.getElementById("views").innerText = data.views;
-  });
-</script>
