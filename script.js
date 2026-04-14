20f9831cdfe9f99679f13fb8c1a68eaa670119a0
Inquieto <nicolanki036@gmail.com>
2026-04-14 14:07:18 -0400

    Update script.js

--- script.js
+++ script.js
@@ -50,11 +50,11 @@ async function updateDiscord() {
     const cover = document.getElementById("cover");
 
     if (user.listening_to_spotify) {
-      text = `🎧 ${user.spotify.song} - ${user.spotify.artist}`;
+      text = ` ${user.spotify.song} - ${user.spotify.artist}`;
       cover.src = user.spotify.album_art_url;
       cover.style.display = "block";
     } else if (user.activities.length > 0) {
-      text = `🎮 ${user.activities[0].name}`;
+      text = ` ${user.activities[0].name}`;
       cover.style.display = "none";
     } else {
       cover.style.display = "none";
